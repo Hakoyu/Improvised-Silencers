59c25c481ae31c78eb124dc37c2f3ac15facb5aa
Hakoyu <Hakoyu@outlook.com>
2026-08-19 12:07:51 +0800

    删除枪械耐久栏的消音器耐久显示, 改为添加到枪械tooltip中

--- Contents/mods/Improvised Silencers/common/media/lua/client/ISIL_InventoryPaneSilencerCondition.lua
+++ Contents/mods/Improvised Silencers/common/media/lua/client/ISIL_InventoryPaneSilencerCondition.lua
@@ -1,7 +1,5 @@
-require "ISUI/ISInventoryPane"
+require "ISUI/ISToolTipInv"
 require "ISIL_Config"
-
-local originalDrawItemDetails = ISInventoryPane.drawItemDetails
 
 local function getAttachedSilencer(item)
     if not item or not instanceof(item, "HandWeapon") or not item.getWeaponPart then
@@ -16,53 +14,83 @@ local function getAttachedSilencer(item)
     return canon
 end
 
-local function getConditionText(item)
+local function getConditionTooltipLine(item)
     if not item or not item.getCondition or not item.getConditionMax then
-        return nil
+        return nil, nil
     end
 
     local condition = ISILSilencerConfig.getItemDurability and ISILSilencerConfig.getItemDurability(item) or item:getCondition()
     local maxCondition = ISILSilencerConfig.getItemMaxDurability and ISILSilencerConfig.getItemMaxDurability(item) or item:getConditionMax()
-    return getText("IGUI_ISIL_Silencer") .. ": " .. tostring(condition) .. "/" .. tostring(maxCondition)
+    local name = item.getDisplayName and item:getDisplayName() or getText("IGUI_ISIL_Silencer")
+
+    return tostring(name) .. ":", tostring(condition) .. "/" .. tostring(maxCondition)
 end
 
-local function drawSilencerCondition(self, item, y, xoff, yoff)
-    local silencer = getAttachedSilencer(item)
-    local conditionText = getConditionText(silencer)
-    if not silencer or not conditionText then
+local function installSilencerConditionTooltipHook()
+    if ISToolTipInv.ISIL_SilencerConditionTooltipHooked then
         return
     end
 
-    local top = self.headerHgt + y * self.itemHgt + yoff
-    local text = getText("IGUI_invpanel_Condition") .. ":"
-    local textWid = getTextManager():MeasureStringX(self.font, text)
-    local weaponBarX = 40 + math.max(120, 30 + textWid + 20) + xoff
-    local weaponBarW = self:getProgressBarWidth()
-    local gap = 8
-    local iconSize = math.max(12, math.min(18, self.itemHgt - 4))
-    local iconX = weaponBarX + weaponBarW + gap
-    local iconY = top + (self.itemHgt - iconSize) / 2
-    local silencerTextX = iconX + iconSize + 5
-    local silencerTextY = top + (self.itemHgt - getTextManager():getFontHeight(self.font)) / 2
-    local silencerTextW = getTextManager():MeasureStringX(self.font, conditionText)
-    local rightPadding = 8
+    ISToolTipInv.ISIL_SilencerConditionTooltipHooked = true
+    local previousRender = ISToolTipInv.render
 
-    if self.width and silencerTextX + silencerTextW > self.width - rightPadding then
-        return
+    function ISToolTipInv:render(...)
+        local silencer = getAttachedSilencer(self.item)
+        local conditionLabel, conditionValue = getConditionTooltipLine(silencer)
+        if not silencer or not conditionLabel or not conditionValue or not self.tooltip then
+            return previousRender(self, ...)
+        end
+
+        local font = UIFont[getCore():getOptionTooltipFont()]
+        local textManager = getTextManager()
+        local lineSpacing = self.tooltip:getLineSpacing()
+        local paddingX = 7
+        local labelWidth = textManager:MeasureStringX(font, conditionLabel)
+        local valueWidth = textManager:MeasureStringX(font, conditionValue)
+        local requiredWidth = labelWidth + valueWidth + paddingX * 3
+        local baseTooltipHeight = nil
+        local tooltipWidth = nil
+
+        local originalSetHeight = self.setHeight
+        local originalSetWidth = self.setWidth
+        local originalDrawRectBorder = self.drawRectBorder
+
+        self.setHeight = function(panel, height, ...)
+            baseTooltipHeight = height
+            return originalSetHeight(panel, height + lineSpacing, ...)
+        end
+
+        self.setWidth = function(panel, width, ...)
+            tooltipWidth = math.max(width, requiredWidth)
+            return originalSetWidth(panel, tooltipWidth, ...)
+        end
+
+        self.drawRectBorder = function(panel, ...)
+            local drawHeight = baseTooltipHeight or math.max(0, (panel.height or 0) - lineSpacing)
+            local drawWidth = tooltipWidth or math.max(panel.width or 0, requiredWidth)
+
+            self.tooltip:DrawText(font, conditionLabel, paddingX, drawHeight - 3, 0.95, 0.95, 0.75, 1)
+            self.tooltip:DrawText(font, conditionValue, drawWidth - valueWidth - paddingX, drawHeight - 3, 1, 1, 1, 1)
+
+            return originalDrawRectBorder(panel, ...)
+        end
+
+        local success, result = pcall(previousRender, self, ...)
+
+        self.setHeight = originalSetHeight
+        self.setWidth = originalSetWidth
+        self.drawRectBorder = originalDrawRectBorder
+
+        if not success then
+            error(result)
+        end
+
+        return result
     end
-
-    local tex = silencer.getTex and silencer:getTex() or nil
-    if tex then
-        self:drawTextureScaled(tex, iconX, iconY, iconSize, iconSize, 1, 1, 1, 1)
-    end
-
-    self:drawText(conditionText, silencerTextX, silencerTextY, 0.8, 0.8, 0.8, 1, self.font)
 end
 
-function ISInventoryPane:drawItemDetails(item, y, xoff, yoff, red)
-    originalDrawItemDetails(self, item, y, xoff, yoff, red)
-
-    if item and instanceof(item, "HandWeapon") then
-        drawSilencerCondition(self, item, y, xoff, yoff)
-    end
+if Events and Events.OnGameStart then
+    Events.OnGameStart.Add(installSilencerConditionTooltipHook)
+else
+    installSilencerConditionTooltipHook()
 end
